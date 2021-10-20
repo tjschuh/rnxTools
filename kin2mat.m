f5e40dfac77ff0a6e01f71f5dab347870a5cbfb9
Terance Schuh <tschuh@princeton.edu>
2021-10-20 00:14:56 -0400

    create a structured MATLAB .mat file from a PRIDE-PPPAR kin file

--- kin2mat.m
+++ kin2mat.m
@@ -36,19 +36,25 @@ matfile=   strip prdfile of its extension
 if not exist(matfile)
     
     % Load the data
+    % file comes in: Mjd, SoD, X, Y, Z, Lat, Lon, Ht, Nsat, PDOP
     dm = load(kinfile);
     
     % Load the header
     try
         h= ;
     catch
-        % Be xeplicit - and be carefil
-        h={'M','x','y'};
+        % Be explicit - and be careful
+        h = {'date','x','y','z','lat','lon','ht','Nsat','PDOP'};
     end
     
-    % Make the time stamps from the column data
-    t=
-
+    % Make the datetime array from the kinfile column data
+    ymd = datestr(dm(:,1)+678942); % convert Mjd to ymd
+    hms = datestr(seconds(dm(:,2)),'HH:MM:SS'); % convert sod to hms
+    for i = 1:length(dm) % need to use for loop here bc dealing with char arrays
+        tstr(i,:) = append(ymd(i,:),' ',hms(i,:)); % combine ymd and hms into one str
+    end
+    t = datetime(tstr,'InputFormat','dd-MMM-yyyy HH:mm:ss'); % convert tstr to datetime
+    
     % Now you make the struct and save for the remainder of the columns
     for index=1:length(h)
         d.(h{index})=dm(:,index+??);
