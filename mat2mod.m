427f5c281fe957878f72e5842b05bfb96569e632
Terance Schuh <tschuh@princeton.edu>
2021-11-15 12:19:22 -0500

    modify data structs from 4 GPS units

--- mat2mod.m
+++ mat2mod.m
@@ -20,6 +20,7 @@ function [d1,d2,d3,d4]=mat2mod(unit1file,unit2file,unit3file,unit4file)
 % d4           modified version of unit4file data struct
 %
 % Originally written by tschuh-at-princeton.edu, 11/12/2021
+% Last modified by tschuh-at-princeton.edu, 11/15/2021
 
 % load in all 4 mat files corresponding to different units
 files = {unit1file,unit2file,unit3file,unit4file};
@@ -45,26 +46,30 @@ end
 % find which d is the smallest and use that one
 % to intersect with all the others so that
 % all 4 datasets start and end at the same time
-for i=1:length(files)
-    lmat(i) = length(dmat(i).t);
-end
-col=find(lmat==min(lmat),1);
-smalld = dmat(col);
-% intersect all the other d's with smalld
-for i=1:length(files)
-    if i ~= col
-        [dmat(i).t,ia,~] = intersect(dmat(i).t,smalld.t);
-        dmat(i).xyz = dmat(i).xyz(ia,:);
-        dmat(i).lat = dmat(i).lat(ia,:);
-        dmat(i).lon = dmat(i).lon(ia,:);
-        dmat(i).utmeasting = dmat(i).utmeasting(ia,:);
-        dmat(i).utmnorthing = dmat(i).utmnorthing(ia,:);
-        dmat(i).utmzone = dmat(i).utmzone(ia,:);
-        dmat(i).height = dmat(i).height(ia,:);
-        dmat(i).nsats = dmat(i).nsats(ia,:);
-        dmat(i).pdop = dmat(i).pdop(ia,:);
+% we do this twice to make the start and end times match
+for j=1:2
+    for i=1:length(files)
+        lmat(i) = length(dmat(i).t);
+    end
+    col=find(lmat==min(lmat),1);
+    smalld = dmat(col);
+    % intersect all the other d's with smalld
+    for i=1:length(files)
+        if i ~= col
+            [dmat(i).t,ia,~] = intersect(dmat(i).t,smalld.t);
+            dmat(i).xyz = dmat(i).xyz(ia,:);
+            dmat(i).lat = dmat(i).lat(ia,:);
+            dmat(i).lon = dmat(i).lon(ia,:);
+            dmat(i).utmeasting = dmat(i).utmeasting(ia,:);
+            dmat(i).utmnorthing = dmat(i).utmnorthing(ia,:);
+            dmat(i).utmzone = dmat(i).utmzone(ia,:);
+            dmat(i).height = dmat(i).height(ia,:);
+            dmat(i).nsats = dmat(i).nsats(ia,:);
+            dmat(i).pdop = dmat(i).pdop(ia,:);
+        end
     end
 end
+
 % finally define d1, d2, d3, d4 from dmat
 d1 = dmat(1);
 d2 = dmat(2);
