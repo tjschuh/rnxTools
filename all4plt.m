fcc13a16e0cc6aa34b78f7316940d4cfe476d534
Terance Schuh <tschuh@princeton.edu>
2021-11-15 17:20:47 -0500

    plot data comparing all 4 GPS units

--- all4plt.m
+++ all4plt.m
@@ -92,7 +92,7 @@ xlim([d1.t(1) d1.t(end)])
 ylabel('Height relative to WGS84 [m]')
 grid on
 longticks
-% to set best ylim remove outliers from alldht
+% to set best ylim, remove outliers from alldht
 % then find the global min
 alldht = rmoutliers(alldht,'mean');
 ylim([min(alldht,[],'all')-0.005*abs(min(alldht,[],'all')) max(alldht,[],'all')+0.005*abs(max(alldht,[],'all'))])
@@ -124,6 +124,7 @@ plot(nbadt4,nbadht4,'color',[0.7 0.7 0.7])
 plot(pbadt4,pbadht4,'color',[0.7 0.7 0.7])
 
 % plot distances between all 4 GPS units
+% grey out bad data?
 dist12 = sqrt((d1.xyz(:,1)-d2.xyz(:,1)).^2 + (d1.xyz(:,2)-d2.xyz(:,2)).^2 + (d1.xyz(:,3)-d2.xyz(:,3)).^2);
 dist13 = sqrt((d1.xyz(:,1)-d3.xyz(:,1)).^2 + (d1.xyz(:,2)-d3.xyz(:,2)).^2 + (d1.xyz(:,3)-d3.xyz(:,3)).^2);
 dist14 = sqrt((d1.xyz(:,1)-d4.xyz(:,1)).^2 + (d1.xyz(:,2)-d4.xyz(:,2)).^2 + (d1.xyz(:,3)-d4.xyz(:,3)).^2);
@@ -145,38 +146,97 @@ ylabel('Distance [m]')
 title('Distances between GPS Receivers')
 grid on
 longticks
-% grey out bad data?
 
 % plot accelerations ax, ay, az
 %ah(3)=subplot(2,2,4);
-% also compute accelerations for GPS 2, 3, and 4
 % compute velocity components vx, vy, vz in m/s
 vx1 = diff(d1.xyz(:,1))./seconds(diff(d1.t));
 vy1 = diff(d1.xyz(:,2))./seconds(diff(d1.t));
 vz1 = diff(d1.xyz(:,3))./seconds(diff(d1.t));
+vx2 = diff(d2.xyz(:,1))./seconds(diff(d2.t));
+vy2 = diff(d2.xyz(:,2))./seconds(diff(d2.t));
+vz2 = diff(d2.xyz(:,3))./seconds(diff(d2.t));
+vx3 = diff(d3.xyz(:,1))./seconds(diff(d3.t));
+vy3 = diff(d3.xyz(:,2))./seconds(diff(d3.t));
+vz3 = diff(d3.xyz(:,3))./seconds(diff(d3.t));
+vx4 = diff(d4.xyz(:,1))./seconds(diff(d4.t));
+vy4 = diff(d4.xyz(:,2))./seconds(diff(d4.t));
+vz4 = diff(d4.xyz(:,3))./seconds(diff(d4.t));
 
 v1 = sqrt(vx1.^2 + vy1.^2 + vz1.^2);
+v2 = sqrt(vx2.^2 + vy2.^2 + vz2.^2);
+v3 = sqrt(vx3.^2 + vy3.^2 + vz3.^2);
+v4 = sqrt(vx4.^2 + vy4.^2 + vz4.^2);
 % 1 knot = 1852 m/hr
 v1knot = 3600*v1/1852;
+v2knot = 3600*v2/1852;
+v3knot = 3600*v3/1852;
+v4knot = 3600*v4/1852;
 
 % compute acceleration components ax, ay, az in m/s^2
 ax1 = diff(vx1)./(2*seconds(diff(d1.t(1:end-1))));
 ay1 = diff(vy1)./(2*seconds(diff(d1.t(1:end-1))));
 az1 = diff(vz1)./(2*seconds(diff(d1.t(1:end-1))));
+ax2 = diff(vx2)./(2*seconds(diff(d2.t(1:end-1))));
+ay2 = diff(vy2)./(2*seconds(diff(d2.t(1:end-1))));
+az2 = diff(vz2)./(2*seconds(diff(d2.t(1:end-1))));
+ax3 = diff(vx3)./(2*seconds(diff(d3.t(1:end-1))));
+ay3 = diff(vy3)./(2*seconds(diff(d3.t(1:end-1))));
+az3 = diff(vz3)./(2*seconds(diff(d3.t(1:end-1))));
+ax4 = diff(vx4)./(2*seconds(diff(d4.t(1:end-1))));
+ay4 = diff(vy4)./(2*seconds(diff(d4.t(1:end-1))));
+az4 = diff(vz4)./(2*seconds(diff(d4.t(1:end-1))));
 
 a1 = sqrt(ax1.^2 + ay1.^2 + az1.^2); 
+a2 = sqrt(ax2.^2 + ay2.^2 + az2.^2); 
+a3 = sqrt(ax3.^2 + ay3.^2 + az3.^2); 
+a4 = sqrt(ax4.^2 + ay4.^2 + az4.^2); 
 
 % how does Frederik want me to plot this exactly?
-% maybe i can plot ax from all 4 units on 1 axis, etc,
+% fix ylim s.t. its symmetric
 figure
-plot(d1.t(1:end-2),ax1,'r')
+int=15;
+subplot(3,1,1)
+plot(d1.t(1:int:end-2),ax1(1:int:end),'r')
 hold on
-plot(d1.t(1:end-2),ay1,'b')
-plot(d1.t(1:end-2),az1,'g')
+plot(d2.t(1:int:end-2),ax2(1:int:end),'g')
+plot(d3.t(1:int:end-2),ax3(1:int:end),'b')
+plot(d4.t(1:int:end-2),ax4(1:int:end),'m')
+legend({'GPS 1','GPS 2','GPS 3','GPS 4'},'Location','northoutside','NumColumns',4)
 grid on
 longticks
 xlim([d1.t(1) d1.t(end-2)])
-ylabel('Acceleration [m/s^2]')
-title('Ship Accelerations')
+ax = [ax1 ax2 ax3 ax4];
+ylim([min(ax,[],'all')-0.005*abs(min(ax,[],'all')) max(ax,[],'all')+0.005*abs(max(ax,[],'all'))])
+ylabel('a_x [m/s^2]')
+xticklabels([])
+
+subplot(3,1,2)
+plot(d1.t(1:int:end-2),ay1(1:int:end),'r')
+hold on
+plot(d2.t(1:int:end-2),ay2(1:int:end),'g')
+plot(d3.t(1:int:end-2),ay3(1:int:end),'b')
+plot(d4.t(1:int:end-2),ay4(1:int:end),'m')
+grid on
+longticks
+xlim([d1.t(1) d1.t(end-2)])
+ay = [ay1 ay2 ay3 ay4];
+ylim([min(ay,[],'all')-0.005*abs(min(ay,[],'all')) max(ay,[],'all')+0.005*abs(max(ay,[],'all'))])
+ylabel('a_y [m/s^2]')
+xticklabels([])
+
+subplot(3,1,3)
+plot(d1.t(1:int:end-2),az1(1:int:end),'r')
+hold on
+plot(d2.t(1:int:end-2),az2(1:int:end),'g')
+plot(d3.t(1:int:end-2),az3(1:int:end),'b')
+plot(d4.t(1:int:end-2),az4(1:int:end),'m')
+grid on
+longticks
+xlim([d1.t(1) d1.t(end-2)])
+az = [az1 az2 az3 az4];
+ylim([min(az,[],'all')-0.005*abs(min(az,[],'all')) max(az,[],'all')+0.005*abs(max(az,[],'all'))])
+ylabel('a_z [m/s^2]')
+%title('Ship Accelerations')
 
 keyboard
