9edc0d9b13e4b83825b78c1a95cb11f2c6551491
Terance Schuh <tschuh@princeton.edu>
2021-11-30 23:27:42 -0500

    compute distance between moving ship and stationary beacon

--- com2dep.m
+++ com2dep.m
@@ -12,8 +12,8 @@ function com2dep(unit1file,unit2file,unit3file,unit4file)
 % OUTPUT:
 %
 %
-%
 % Originally written by tschuh-at-princeton.edu, 11/24/2021
+% Last modified by tschuh-at-princeton.edum 11/30/2021
 
 % set beacon location on seafloor
 % these come from file 05040 first xyz positions, but z-5km
@@ -27,23 +27,29 @@ dogz = 3.30385e6; %~5 km below surface
 d = mat2com(unit1file,unit2file,unit3file,unit4file,0);
 [~,fname,~] = fileparts(unit1file);
 
-% calculate distance between ship and beacon for each second in meters
-dist = sqrt((d.x(:) - dogx).^2 + (d.y(:) - dogy).^2 + (d.z(:) - dogz).^2);
+% calculate slant range between ship and beacon for each second in meters
+sr = sqrt((d.x(:) - dogx).^2 + (d.y(:) - dogy).^2 + (d.z(:) - dogz).^2);
+
+% constant sound speed profile for now [m/s]
+v = 1500;
+
+% calculate slant time from slant range and v [s]
+st = sr./v;
 
 % plot the distance [km] vs time
 % would be nice to eventually add error bars
-plot(d.t,dist*1e-3,'LineWidth',2)
+plot(d.t,sr*1e-3,'LineWidth',2)
 %set(gca,'YDir','reverse')
 xlim([d.t(1) d.t(end)])
 buff = 0.01;
-ylim([min(dist*1e-3)-buff*min(dist*1e-3) max(dist*1e-3)+buff*max(dist*1e-3)])
+ylim([min(sr*1e-3)-buff*min(sr*1e-3) max(sr*1e-3)+buff*max(sr*1e-3)])
 ylabel('Slant Range [km]')
 title(sprintf('Raw Slant Range Measurements: %s',datestr(d.t(1))))
 grid on
 longticks
-
+keyboard
 % save figure as pdf
-figdisp(fname,[],'',2,[],'epstopdf')
+%figdisp(fname,[],'',2,[],'epstopdf')
 
 % close figure
-close
+%close
