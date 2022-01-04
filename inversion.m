fe274c0baabe995296b6dccc947e7befac9bb721
Terance Schuh <tschuh@princeton.edu>
2022-01-04 18:57:45 -0500

    use trilateration method to find beacon location

--- inversion.m
+++ inversion.m
@@ -1,6 +1,6 @@
-function [sol1,sol2,real] = inversion(unit1file,unit2file,unit3file,unit4file)
+function [sol,tru] = inversion(unit1file,unit2file,unit3file,unit4file)
 %
-% using trilateration method (not my code) calculate beacon
+% use trilateration method (not my code) to calculate beacon
 % location on seafloor from slant ranges and ship locations
 % can also use RecTrilateration.m to see if it improves result
 %
@@ -13,11 +13,11 @@ function [sol1,sol2,real] = inversion(unit1file,unit2file,unit3file,unit4file)
 %
 % OUTPUT:
 %
-% sol1          calculated average [x y z] beacon location [m] (method 1)
-% sol2          calculated average [x y z] beacon location [m] (method 2)
-% real          true [x y a] beacon location [m]
+% sol           calculated [x y z] beacon location [m]
+% tru           actual [x y z] beacon location [m]
 %
 % Originally written by tschuh-at-princeton.edu, 11/30/2021
+% Last modified by tschuh-at-princeton.edu, 01/04/2022
 
 % use mat2mod to convert data to all be same time spans with no time gaps
 [d1,d2,d3,d4] = mat2mod(unit1file,unit2file,unit3file,unit4file);
@@ -27,7 +27,7 @@ function [sol1,sol2,real] = inversion(unit1file,unit2file,unit3file,unit4file)
 dogx = 1.979e6;
 dogy = -5.074e6;
 dogz = 3.30385e6; %~5 km below surface
-real = [dogx dogy dogz];
+tru = [dogx dogy dogz];
 
 % find a location (x,y,z) for every second 
 for i=1:length(d1.xyz)
@@ -49,17 +49,86 @@ for i=1:length(d1.xyz)
 
     % if there are any NaN values, dont do Trilateration, it wont run
     if isnan(sr1)==1 | isnan(sr2)==1 | isnan(sr3)==1
-        N1mat(i,:) = [NaN NaN NaN];
-        N2mat(i,:) = [NaN NaN NaN];
+        solmat(i,:) = [NaN NaN NaN];
     else
         % use Trilateration code from "An Algebraic Solution to the Multilateration Problem"
-        [N1 N2] = Trilateration(P,S, diag(ones(1,3)));
-        N1mat(i,:) = [N1(2,1) N1(3,1) N1(4,1)];
-        N2mat(i,:) = [N2(2,1) N2(3,1) N2(4,1)];
+        % soltype = 1 or 2 (2 is more accurate)
+        soltype = 2;
+        sol = Trilateration(P,S,diag(ones(1,3)),soltype);
+        solmat(i,:) = [sol(2,1) sol(3,1) sol(4,1)];
     end
 end
 
 % for final location take average of all locations (maybe not best way)
 % could make histogram here
-sol1 = nanmean(N1mat,1);
-sol2 = nanmean(N2mat,1);
+sol = real(nanmean(solmat,1));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function sol = Trilateration(P,S,W,soltype)
+
+% np = # of stations
+% ns = # of slant ranges
+[mp,np] = size(P);
+ns = length(S);
+% np and ns must match
+if (ns ~= np)
+    error('Number of reference points and distances are different');
+end
+
+A=[]; b=[];
+for i1=1:np
+    x = P(1,i1); y = P(2,i1); z = P(3,i1);
+    s = S(i1);
+    A = [A ; 1 -2*x  -2*y  -2*z]; 
+    b= [b ; s^2-x^2-y^2-z^2 ];
+end
+
+if np == 3
+    % Gaussian elimination
+    Xp= A\b;
+    % or Xp=pinv(A)*b; 
+    % the matrix  inv(A'*A)*A' or inv(A'*C*A)*A'*C or pinv(A)
+    % depend only on the reference points
+    % it could be computed only once
+    xp = Xp(2:4,:);
+    Z = null(A,'r');
+    z = Z(2:4,:);
+    if rank(A) == 3
+        % Polynomial coefficients
+        a2 = z(1)^2 + z(2)^2 + z(3)^2 ;
+        a1 = 2*(z(1)*xp(1) + z(2)*xp(2) + z(3)*xp(3))-Z(1);
+        a0 = xp(1)^2 +  xp(2)^2+  xp(3)^2-Xp(1);
+        p = [a2 a1 a0];
+        t = roots(p);
+
+        % Solutions
+        if soltype == 1
+            sol = Xp + t(1)*Z;
+        elseif soltype == 2    
+            sol = Xp + t(2)*Z;
+        end
+    end
+
+elseif np > 3
+% Particular solution
+
+    if W ~= diag(ones(1,length(W)))
+        C = W'*W;
+        Xpdw = inv(A'*C*A)*A'*C*b; % Solution with Weights Matrix
+    else
+        Xpdw = pinv(A)*b; % Solution without Weights Matrix
+    end
+ 
+    % the matrix  inv(A'*A)*A' or inv(A'*C*A)*A'*C or pinv(A)
+    % depend only on the reference points
+    % it could be computed only once
+    if soltype == 1
+        sol = Xpdw;
+    elseif soltype == 2
+        sol = Xpdw;
+    end
+
+else % np <= 3
+    error('Not enough stations');
+end
