4aae1db3dade5e73a324ba0304400f8ee4658ad5
Frederik J. Simons <fjsimons@gmail.com>
2022-01-20 00:20:34 -0500

    FJS version

--- gpsdiagram.m
+++ gpsdiagram.m
@@ -17,9 +17,9 @@ function gpsdiagram(dist12,dist13,dist14,dist23,dist24,dist34)
 % plot of diagram
 %
 % Last modified by tschuh-at-princeton.edu, 01/12/2022
-% Last modified by fjsimons-at-princeton.edu, 01/19/2022
+% Last modified by fjsimons-at-princeton.edu, 01/20/2022
 
-% values from R/V Atlantic Explorer
+% Values from R/V Atlantic Explorer
 defval('dist12', 4.8)
 defval('dist13',12.2)
 defval('dist14',10.1)
@@ -27,71 +27,88 @@ defval('dist23',10.3)
 defval('dist24',11.3)
 defval('dist34', 7.1)
 
-% percentage threshold for calculation approximation in if statement
+% Determine GPS relative coordinates relative to receiver 4
+gps(1,:) = [0      dist14];
+gps(2,:) = [dist12 dist14];
+gps(3,:) = [dist34 0     ];
+gps(4,:) = [0      0     ];
+
+% Find out where the diagonals (1,3) and (2,4) cross and use that as a reference
+% https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+D =[gps(1,1)-gps(3,1)]*[gps(2,2)-gps(4,2)]-[gps(1,2)-gps(3,2)]*[gps(2,1)-gps(4,1)];
+xo=([gps(1,1)*gps(3,2)-gps(1,2)*gps(3,1)]*[gps(2,1)-gps(4,1)]...
+    -[gps(1,1)-gps(3,1)]*[gps(2,1)*gps(4,2)-gps(2,2)*gps(4,1)])/D;
+yo=([gps(1,1)*gps(3,2)-gps(1,2)*gps(3,1)]*[gps(2,2)-gps(4,2)]...
+    -[gps(1,2)-gps(3,2)]*[gps(2,1)*gps(4,2)-gps(2,2)*gps(4,1)])/D;
+        
+% Percentage threshold for calculation approximation in if statement
 pct = 0.05;
-% offset to add to coordinates for neater plotting
-offset = [1 1];
 
-% Font size of box tex
+% Receiver symbols and colors
+sym='o'; 
+me='k';
+mf=grey;
+
+% Font size of text boxes
 fs=10;
+% Offset for these text boxes
+to=[0 1 ; 0 1 ; 0 -1 ; 0 -1]/1.5;
+% Offset for axis limits
+offset = [1.5 0.75];
 
-% Text box locations
-tx=[0.15 0.22 0 0.7;
-    0.60 0.22 0 0.7;
-    0.80 0.18 0 0  ; 
-    0.15 0.18 0 0  ;
-    0.48 0.80 0 0  ;
-    0.48 0.25 0 0  ;
-    0.30 0.55 0 0  ;
-    0.52 0.55 0 0 ];
-% Text box strings
-ts={'Unit 1','Unit 2','Unit 3','Unit 4','bow','stern','port','starboard'};
+% Special box strings
+ts={'bow','stern','port','starboard'};
+% Offset for these boxes
+os=[0 3 ; 0 -4 ; -1.5 0 ; 1.5 0];
+% Alignment for these boxes
+as={'center','center','right','left'};
 
 % if pythagorean thm holds for the given distances, then this
 % must be the configuration we want so go ahead and plot
 if abs((dist24)^2 - ((dist12)^2 + (dist14)^2)) <= pct*(dist24)^2 ...
         && abs((dist13)^2 - ((dist14)^2 + (dist34)^2)) <= pct*(dist13)^2
-    % determine relative GPS coordinates
-    gps1 = [0      dist14] + offset;
-    gps2 = [dist12 dist14] + offset;
-    gps3 = [dist34 0     ] + offset;
-    gps4 = [0      0     ] + offset;
-    % combine all coordinates to plot connecting lines
-    all4 = [gps1; gps2; gps3; gps4; gps1];
-
     clf
-    % main plotting
-    scatter(gps1(1),gps1(2),'b','filled')
     hold on
-    scatter(gps2(1),gps2(2),'b','filled')
-    scatter(gps3(1),gps3(2),'b','filled')
-    scatter(gps4(1),gps4(2),'b','filled')
-    plot(all4(:,1),all4(:,2),'k')
-    tl=title('Diagram of 4 GPS Locations');
-    xlim([0 gps3(1)+offset(2)])
-    ylim([0 gps1(2)+offset(1)])
-    xlabel('distance [m]')
-    ylabel('distance [m]')
+    % Plot the straight connecting lines
+    ps=plot(gps([1 2 3 4 1],1),gps([1 2 3 4 1],2),'k');
+    % Plot the diagonal connecting lines
+    pd=plot(gps([1 3 2 4],1),gps([1 3 2 4],2),'k');
     
-    % extra annotation
-
-    annotation('arrow',[0.47 0.47],[0.55 0.75]);
-    annotation('arrow',[0.47 0.47],[0.45 0.25]);
-
-    for index=1:size(tx,1)
-      xa{index} = annotation('textbox',tx(index,:),'String',ts{index},...
-			     'FontSize',fs,'FitBoxToText','on');
+    % Plotting of the GPS statioons, on top!
+    hold on
+    for index=1:4
+      pg=plot(gps(index,1),gps(index,2),sym,'MarkerEdgeColor',me,'MarkerFaceColor',mf);
+    end
+    
+    % Text boxes identifying the receivers 
+    for index=1:size(gps,1)
+      xa(index)=boxtex(gps(index,:)+to(index,:),[],index,fs,2);
     end
     hold off
-    grid on
+
+    % grid on
+    % if grid on neex to use BOXTEX but take the boxes away... so the
+    % white covers the grid lines....
+
+    % Now identify the nautical terms relative to everything else
+    for index=1:length(ts)
+      ta(index)=text(xo+os(index,1),yo+os(index,2),ts{index});
+      set(ta(index),'HorizontalAlignment',as{index},'FontSize',fs+2)
+    end    
+    
+    % Cosmetics and cleanup
+    xlim([0 gps(3,1)]+[-1 1]*offset(2))
+    ylim([0 gps(1,2)]+[-1 1]*offset(1))
+    xlabel('distance [m]')
+    ylabel('distance [m]')
     longticks(gca,2)
+    box on
+    
+    % Didn't like the first and last two at some point
+    % xt=get(gca,'XTick'); set(gca,'XTick',xt(2:end-1));
 else
     error('Cannot compute configuration currently')
 end
-
-% Final cleanup
-delete(tl)
-box on
 
 % Print to PDF
 figdisp([],[],[],2)
