496f17697bb952f370f086dc469d87ef1fa04ef2
Frederik J. Simons <fjsimons@gmail.com>
2022-01-19 23:05:36 -0500

    FJS cleanup

--- gpsdiagram.m
+++ gpsdiagram.m
@@ -17,65 +17,82 @@ function gpsdiagram(dist12,dist13,dist14,dist23,dist24,dist34)
 %
 % plot of diagram
 %
-% Originally written by tschuh-at-princeton.edu, 01/12/2022
+% Last modified by tschuh-at-princeton.edu, 01/12/2022
+% Last modified by fjsimons-at-princeton.edu, 01/19/2022
 
 % values from R/V Atlantic Explorer
-defval('dist12',4.8)
+defval('dist12', 4.8)
 defval('dist13',12.2)
 defval('dist14',10.1)
 defval('dist23',10.3)
 defval('dist24',11.3)
-defval('dist34',7.1)
+defval('dist34', 7.1)
 
 % percentage threshold for calculation approximation in if statement
 pct = 0.05;
 % offset to add to coordinates for neater plotting
 offset = [1 1];
 
+% Font size of box
+fs=8;
+
+% Text box locations
+tx=[0.15 0.22 0 0.7;
+    0.60 0.22 0 0.7;
+    0.80 0.18 0 0  ; 
+    0.15 0.18 0 0  ;
+    0.48 0.80 0 0  ;
+    0.48 0.25 0 0  ;
+    0.30 0.55 0 0  ;
+    0.52 0.55 0 0 ];
+% Text box strings
+ts={'Unit 1','Unit 2','Unit 3','Unit 4','bow','stern','port','starboard'};
+
 % if pythagorean thm holds for the given distances, then this
 % must be the configuration we want so go ahead and plot
 if abs((dist24)^2 - ((dist12)^2 + (dist14)^2)) <= pct*(dist24)^2 ...
         && abs((dist13)^2 - ((dist14)^2 + (dist34)^2)) <= pct*(dist13)^2
     % determine relative GPS coordinates
-    gps1 = [0 dist14] + offset;
+    gps1 = [0      dist14] + offset;
     gps2 = [dist12 dist14] + offset;
-    gps3 = [dist34 0] + offset;
-    gps4 = [0 0] + offset;
+    gps3 = [dist34 0     ] + offset;
+    gps4 = [0      0     ] + offset;
     % combine all coordinates to plot connecting lines
     all4 = [gps1; gps2; gps3; gps4; gps1];
 
-    % plotting
+    clf
+    % main plotting
     scatter(gps1(1),gps1(2),'b','filled')
     hold on
     scatter(gps2(1),gps2(2),'b','filled')
     scatter(gps3(1),gps3(2),'b','filled')
     scatter(gps4(1),gps4(2),'b','filled')
     plot(all4(:,1),all4(:,2),'k')
-    title('Diagram of 4 GPS Locations')
+    tl=title('Diagram of 4 GPS Locations');
     xlim([0 gps3(1)+offset(2)])
-    xlabel('Distance [meters]')
     ylim([0 gps1(2)+offset(1)])
-    ylabel('Distance [meters]')
-    a = annotation('textbox',[0.15 0.22 0 0.7],'String',['Unit 1'],'FitBoxToText','on');
-    a.FontSize = 8;
-    b = annotation('textbox',[0.6 0.22 0 0.7],'String',['Unit 2'],'FitBoxToText','on');
-    b.FontSize = 8;
-    c = annotation('textbox',[0.8 0.18 0 0],'String',['Unit 3'],'FitBoxToText','on');
-    c.FontSize = 8;
-    d = annotation('textbox',[0.15 0.18 0 0],'String',['Unit 4'],'FitBoxToText','on');
-    d.FontSize = 8;
-    e = annotation('textbox',[0.48 0.8 0 0],'String',['bow'],'FitBoxToText','on');
-    e.FontSize = 8;
-    f = annotation('textbox',[0.48 0.25 0 0],'String',['stern'],'FitBoxToText','on');
-    f.FontSize = 8;
+    xlabel('distance [m]')
+    ylabel('distance [m]')
+    
+    % extra annotation
+
     annotation('arrow',[0.47 0.47],[0.55 0.75]);
     annotation('arrow',[0.47 0.47],[0.45 0.25]);
-    h = annotation('textbox',[0.3 0.55 0 0],'String',['portside'],'FitBoxToText','on');
-    h.FontSize = 8;
-    g = annotation('textbox',[0.52 0.55 0 0],'String',['starboard'],'FitBoxToText','on');
-    g.FontSize = 8;
+
+    for index=1:size(tx,1)
+      xa{index} = annotation('textbox',tx(index,:),'String',ts{index},...
+			     'FontSize',fs,'FitBoxToText','on');
+    end
+    hold off
     grid on
-    longticks
+    longticks(gca,2)
 else
     error('Cannot compute configuration currently')
 end
+
+% Final cleanup
+delete(tl)
+box on
+
+% Print to PDF
+figdisp([],[],[],2)
