d3df20c28d0acc61ce4e9bf256a8b81b538ab32c
Frederik J. Simons <fjsimons@gmail.com>
2022-01-31 15:33:00 -0500

    Equalizes observation epochs between MAT files

--- mat2mod.m
+++ mat2mod.m
@@ -22,23 +22,24 @@ function varargout=mat2mod(files)
 % Last modified by tschuh-at-princeton.edu, 11/15/2021
 % Last modified by fjsimons-at-alum.mit.edu, 01/31/2022
 
+% Non-array variables to exclude from the tabling procedure
+drem={'xyzunit','lonlatunit','utmunit','heightunit','satlabels'};
+
 for i=1:length(files)
     load(files{i});
-    % Use timetable to fill in time skips/data gaps
-    tt = timetable(d.t,d.xyz,d.lat,d.lon,d.utmeasting,d.utmnorthing,d.utmzone,d.height,d.nsats,d.pdop);
-    rett = retime(tt,'secondly','fillwithmissing');
-    % redefine struct fields with NaN rows included
-    % I would do a loop here using fieldnames
-    d.t = rett.Time;
-    d.xyz = rett.Var1;
-    d.lat = rett.Var2;
-    d.lon = rett.Var3;
-    d.utmeasting = rett.Var4;
-    d.utmnorthing = rett.Var5;
-    d.utmzone = rett.Var6;
-    d.height = rett.Var7;
-    d.nsats = rett.Var8;
-    d.pdop = rett.Var9;
+    % Only get the wanted variables, turn them into a timetable
+    % Use (RE)TIME(TABLE) to fill in time skips/data gaps with NaNs
+    tt=retime(table2timetable(struct2table(rmfield(d,drem))),'secondly','fillwithmissing');
+    % Reassign to the old STRUCT
+    d.t=tt.t;
+    % These are the variables we kept
+    fnd=fieldnames(rmfield(d,drem));
+    % And they appear shifted by one in the time table
+    fnt=fieldnames(tt);
+    % So now you put them back in the right place as a struct
+    for j=2:length(fn)
+      d.(fn{j})=tt.(fnt{j-1});
+    end    
     % Assemble for later use
     dmat(i) = d;
 end
@@ -52,20 +53,14 @@ for j=1:2
     lmat(i) = length(dmat(i).t);
   end
   col=find(lmat==min(lmat),1);
-  smalld = dmat(col);
   % intersect all the other d's with smalld
   for i=1:length(files)
     if i ~= col
-      [dmat(i).t,ia,~] = intersect(dmat(i).t,smalld.t);
-      dmat(i).xyz = dmat(i).xyz(ia,:);
-      dmat(i).lat = dmat(i).lat(ia,:);
-      dmat(i).lon = dmat(i).lon(ia,:);
-      dmat(i).utmeasting = dmat(i).utmeasting(ia,:);
-      dmat(i).utmnorthing = dmat(i).utmnorthing(ia,:);
-      dmat(i).utmzone = dmat(i).utmzone(ia,:);
-      dmat(i).height = dmat(i).height(ia,:);
-      dmat(i).nsats = dmat(i).nsats(ia,:);
-      dmat(i).pdop = dmat(i).pdop(ia,:);
+      % Do this for all field names
+      [~,ia,~] = intersect(dmat(i).t,dmat(col).t);
+      for k=1:length(fn)
+	dmat(i).(fn{k}) = dmat(i).(fn{k})(ia,:);
+      end
     end
   end
 end
