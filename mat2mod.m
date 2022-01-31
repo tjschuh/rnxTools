af56183a35c0ed54aad9886f48ff0df079fadfef
Frederik J. Simons <fjsimons@gmail.com>
2022-01-31 14:40:06 -0500

    Equalizes observation epochs between MAT files

--- mat2mod.m
+++ mat2mod.m
@@ -1,35 +1,34 @@
-function [d1,d2,d3,d4]=mat2mod(unit1file,unit2file,unit3file,unit4file)
-% [d1,d2,d3,d4]=MAT2MOD(unit1file,unit2file,unit3file,unit4file)
+function varargout=mat2mod(files)
+% dmat=MAT2MOD(files)
 %
-% takes in data from 4 different units
-% makes them all start and end at the same time
-% and inserts NaNs for times where no data was processed
+% Given Precise Point Position time series of (four) different units, makes
+% them all start and end at the same time and inserts NaNs for times where
+% no data were processed
 %
 % INPUT:
-%
-% unit1file     mat file containing data collected by unit 1
-% unit2file     mat file containing data collected by unit 2
-% unit3file     mat file containing data collected by unit 3
-% unit4file     mat file containing data collected by unit 4
+% 
+% files        cell with MAT-filename strings containing data
 %
 % OUTPUT:
 %
-% d1           modified version of unit1file data struct
-% d2           modified version of unit2file data struct
-% d3           modified version of unit3file data struct
-% d4           modified version of unit4file data struct
+% dmat         cell with modified versions of data structure
 %
+% EXAMPLE:
+%
+% files = {unit1file,unit2file,unit3file,unit4file};
+% d=mat2mod(files);
+% 
 % Originally written by tschuh-at-princeton.edu, 11/12/2021
 % Last modified by tschuh-at-princeton.edu, 11/15/2021
+% Last modified by fjsimons-at-alum.mit.edu, 01/31/2022
 
-% load in all 4 mat files corresponding to different units
-files = {unit1file,unit2file,unit3file,unit4file};
 for i=1:length(files)
     load(files{i});
-    % use timetable to fill in time skips/data gaps
+    % Use timetable to fill in time skips/data gaps
     tt = timetable(d.t,d.xyz,d.lat,d.lon,d.utmeasting,d.utmnorthing,d.utmzone,d.height,d.nsats,d.pdop);
     rett = retime(tt,'secondly','fillwithmissing');
     % redefine struct fields with NaN rows included
+    % I would do a loop here using fieldnames
     d.t = rett.Time;
     d.xyz = rett.Var1;
     d.lat = rett.Var2;
@@ -40,6 +39,7 @@ for i=1:length(files)
     d.height = rett.Var7;
     d.nsats = rett.Var8;
     d.pdop = rett.Var9;
+    % Assemble for later use
     dmat(i) = d;
 end
 
@@ -48,30 +48,28 @@ end
 % all 4 datasets start and end at the same time
 % we do this twice to make the start and end times match
 for j=1:2
-    for i=1:length(files)
-        lmat(i) = length(dmat(i).t);
+  for i=1:length(files)
+    lmat(i) = length(dmat(i).t);
+  end
+  col=find(lmat==min(lmat),1);
+  smalld = dmat(col);
+  % intersect all the other d's with smalld
+  for i=1:length(files)
+    if i ~= col
+      [dmat(i).t,ia,~] = intersect(dmat(i).t,smalld.t);
+      dmat(i).xyz = dmat(i).xyz(ia,:);
+      dmat(i).lat = dmat(i).lat(ia,:);
+      dmat(i).lon = dmat(i).lon(ia,:);
+      dmat(i).utmeasting = dmat(i).utmeasting(ia,:);
+      dmat(i).utmnorthing = dmat(i).utmnorthing(ia,:);
+      dmat(i).utmzone = dmat(i).utmzone(ia,:);
+      dmat(i).height = dmat(i).height(ia,:);
+      dmat(i).nsats = dmat(i).nsats(ia,:);
+      dmat(i).pdop = dmat(i).pdop(ia,:);
     end
-    col=find(lmat==min(lmat),1);
-    smalld = dmat(col);
-    % intersect all the other d's with smalld
-    for i=1:length(files)
-        if i ~= col
-            [dmat(i).t,ia,~] = intersect(dmat(i).t,smalld.t);
-            dmat(i).xyz = dmat(i).xyz(ia,:);
-            dmat(i).lat = dmat(i).lat(ia,:);
-            dmat(i).lon = dmat(i).lon(ia,:);
-            dmat(i).utmeasting = dmat(i).utmeasting(ia,:);
-            dmat(i).utmnorthing = dmat(i).utmnorthing(ia,:);
-            dmat(i).utmzone = dmat(i).utmzone(ia,:);
-            dmat(i).height = dmat(i).height(ia,:);
-            dmat(i).nsats = dmat(i).nsats(ia,:);
-            dmat(i).pdop = dmat(i).pdop(ia,:);
-        end
-    end
+  end
 end
 
-% finally define d1, d2, d3, d4 from dmat
-d1 = dmat(1);
-d2 = dmat(2);
-d3 = dmat(3);
-d4 = dmat(4);
+% Variable output
+varns={dmat};
+varargout=varns(1:nargout);
